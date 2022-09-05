f561d1b08f12e9c16c36cc64001b51c5e99cd685
ErickGX <ericksaopaulino1@hotmail.com>
2022-09-05 18:18:16 -0300

    Criando classe de conexao com banco de dados orientado a objetos PDO+POO

---
+++ WEB-ARMARIO/pdo_poo_back-end/conexaoBD.php
@@ -0,0 +1,54 @@
+<?php
+
+// //conectando ao BD versao antiga sem POO
+
+// try{
+//   $pdo = new PDO('mysql:host=localhost;dbname=dbarmariov3;charset=utf8', 'root','');
+
+//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//   $output = 'Conexão estabelecida. <br>';
+// }catch(PDOException $errobd) {
+//     $output = 'Impossivel conectar BD : ' . $errobd . '<br>';
+// }
+// //echo $output;
+
+
+
+///classe de conexao com banco de dados
+class conexaobd
+{
+
+    private $_servername;
+    private $_username;
+    private $_password;
+    private $_dbname;
+
+    public $_conexao;
+
+
+    //função para instanciar a conexao com BD
+    function __construct($server, $user, $passwd, $db)
+    {
+        $this->_servername = $server;
+        $this->_username = $user;
+        $this->_password = $passwd;
+        $this->_dbname = $db;
+
+
+
+
+        try {
+            $this->_conexao = new PDO("mysql:host=$this->_servername; dbname=$this->_dbname; charset=utf8", $this->_username, $this->_password);
+
+            $this->_conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->_conexao->exec("set names utf8");
+
+            echo "conexao estabelecida";
+        } catch (PDOException $erro) {
+            echo "Erro na conexão:" . $erro->getMessage();
+        }
+    }
+
+    public  function 
+}
